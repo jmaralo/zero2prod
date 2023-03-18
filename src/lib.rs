8cf57d4867ab18b5de31d41585df7caebf2d4741
jmaralo <martinezalonsojn@gmail.com>
2023-03-18 21:21:58 +0100

    Fix formatting
    Previous commit didn't follow formatting rules

--- src/lib.rs
+++ src/lib.rs
@@ -10,9 +10,11 @@ use hyper::server::conn::AddrIncoming;
 pub fn run(listener: TcpListener) -> Server<AddrIncoming, IntoMakeService<Router>> {
     let app = Router::new().route("/health_check", get(health_check));
 
-    axum::Server::from_tcp(listener).unwrap().serve(app.into_make_service())
+    axum::Server::from_tcp(listener)
+        .unwrap()
+        .serve(app.into_make_service())
 }
 
 async fn health_check() -> StatusCode {
-    return StatusCode::OK;
+    StatusCode::OK
 }
