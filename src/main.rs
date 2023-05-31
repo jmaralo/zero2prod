900b999398f7e9a78c0d829238bf57e2b6834961
jmaralo <martinezalonsojn@gmail.com>
2023-06-01 01:48:50 +0200

    DEV print configuration when running

--- src/main.rs
+++ src/main.rs
@@ -16,9 +16,13 @@ async fn main() {
     let addr = format!("{}:{}", config.application.host, config.application.port);
     let listener = TcpListener::bind(addr).unwrap();
 
+    let options = config.database.with_db();
+    println!("{:?}", options);
+    println!("{:?}", config);
+
     let db_pool = PgPoolOptions::new()
         .acquire_timeout(Duration::from_secs(2))
-        .connect_lazy_with(config.database.with_db());
+        .connect_lazy_with(options);
 
     run(listener, db_pool).await.unwrap();
 }
